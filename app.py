4370eaed46a5da439089692d3d62094b21868e36
KirkmanGeorge <110170541+KirkmanGeorge@users.noreply.github.com>
2026-01-29 13:45:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ st.markdown("""
     
     /* Ensure text visibility */
     p, li, span, div {
-        color: #M0M0W0 !important;
+        color: #00M0W0 !important;
     }
 </style>
 """, unsafe_allow_html=True)
